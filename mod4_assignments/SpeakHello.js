f4e39b2b07637bd582feb82b719a0bb6be7838c7
Erick <contato.bioerick@gmail.com>
2024-02-04 17:21:47 -0300

    finished mod4 assingment

---
+++ mod4_assignments/SpeakHello.js
@@ -0,0 +1,10 @@
+var helloSpeaker = {};
+
+(function(window) {
+	var speakWord = "Hello";
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+})(window);
